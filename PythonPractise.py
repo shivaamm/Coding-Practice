80dbc9fdc96081d0a8e42f9d38e8dce190470cd4
Shivam Singhal <shivam.singhal04888@gmail.com>
2020-09-14 15:13:07 +0530

    python file

--- PythonPractise.py
+++ PythonPractise.py
@@ -5,3 +5,24 @@ print(a)
 
 
 print('I am {} years old.'.format(str(19)))
+
+spam = 0
+while spam < 5:
+    print('Hello, world.')
+    spam = spam + 1
+
+
+if n%2==0:
+    print("Even")
+else:
+    print("Odd")
+
+fact=1    
+n=int(input())
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
+
+
+    
+
