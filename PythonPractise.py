2525e54399d667308e63cb0ee61a2a28f9d983c6
Shivam Singhal <shivam.singhal04888@gmail.com>
2020-09-14 16:28:03 +0530

    python file

--- PythonPractise.py
+++ PythonPractise.py
@@ -1,28 +1,84 @@
-print("hello")
+###print("hello")
+###
+###a= (5 - 1) * ((7 + 1) / (3 - 1))
+###print(a)
+###
+###
+###print('I am {} years old.'.format(str(19)))
+###
+###spam = 0
+###while spam < 5:
+###    print('Hello, world.')
+###    spam = spam + 1
+###
+###
+###if n%2==0:
+###    print("Even")
+###else:
+###    print("Odd")
+###
+###fact=1    
+###n=int(input())
+###for i in range(1,n+1):
+###    fact=fact*i
+###print(fact)
+###
+##
+##n = int(input())
+##m = int(input())
+##o = int(input())
+##
+##if n>m and n>o:
+##    print(n)
+##elif m>n and m>o:
+##    print(m)
+##else:
+##    print(o)
+#
+#Pattern 4
 
-a= (5 - 1) * ((7 + 1) / (3 - 1))
-print(a)
+for i in range(65,70):
+    for j in range(1,6):
+        print(chr(i), end=" ")
+    print()
+
+for i in range(65,70):
+    for j in range(1,6):
+        print(chr(j), end=" ")
+    print()
+
+print("--------------")
+for i in range(6,1,-1):
+    for j in range(70,65,-1):
+        print(chr(j), end=" ")
+    print()
+
+print("--------------")
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i, end=" ")
+    print()
 
 
-print('I am {} years old.'.format(str(19)))
+print("--------------")
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end=" ")
+    print()
 
-spam = 0
-while spam < 5:
-    print('Hello, world.')
-    spam = spam + 1
-
-
-if n%2==0:
-    print("Even")
-else:
-    print("Odd")
-
-fact=1    
-n=int(input())
-for i in range(1,n+1):
-    fact=fact*i
-print(fact)
+print("--------------")
+for i in range(1,6):
+    for j in range(6,i,-1):
+        print(i, end=" ")
+    print()
 
 
-    
+
+
+
+
+
+
+
+
 
