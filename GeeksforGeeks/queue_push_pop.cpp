676b43e7b747cd34ef76876a5914ae7cd8185a66
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-12 02:58:07 +0530

    Create queue_push_pop.cpp

---
+++ GeeksforGeeks/queue_push_pop.cpp
@@ -0,0 +1,20 @@
+queue<int>_push(int arr[],int n)
+{
+   //return a queue with all elements of arr inserted in it
+   queue<int> q;
+   for(int i=0;i<n;i++)
+   {
+       q.push(arr[i]);
+   }
+   return q;
+}
+
+void _pop(queue<int>s)
+{
+    //print front and dequeue for each element until it becomes empty
+    while(!s.empty())
+    {
+        cout<<s.front()<<" ";
+        s.pop();
+    }
+} 
