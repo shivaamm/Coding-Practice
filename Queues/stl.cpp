9d69174a02f09594121544d482a6b56cfb86a2a7
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-12 00:00:06 +0530

    Create stl.cpp

---
+++ Queues/stl.cpp
@@ -0,0 +1,6 @@
+Functions used here:
+   q.size() = Returns the size of queue.
+   q.push() = It is used to insert elements to the queue.
+   q.pop() = To pop out the value from the queue.
+   q.front() = Returns the front element of the array.
+   q.back() = Returns the back element of the array.
