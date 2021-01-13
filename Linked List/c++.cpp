06f11623f7bd437bee2f14cdae19924b4667f7a8
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-14 04:42:29 +0530

    c++ code with c code comments

---
+++ Linked List/c++.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+using namespace std;
+// #include<malloc.h/>
+struct node
+{
+	int data;
+	node*next;
+};
+struct node*head,*temp,*newnode;
+void create()
+{
+	int n,i;
+// 	scanf("%d",&n);
+    cin>>n;
+	head=NULL;
+	for(i=0;i<n;i++)
+	{
+// 		newnode=(struct node*)malloc(sizeof(struct node));
+        newnode = new node;
+// 		scanf("%d",&newnode->data);
+        cin>>newnode->data;
+		newnode->next=NULL;
+		
+		if(head==NULL)
+		{
+			head=newnode;
+		}
+		else
+		{
+		temp=head;
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+			}	
+			temp->next=newnode;
+		}
+	}
+}
+// void insert1()
+// {
+// 	/*struct node;
+// 	{
+// 		int data;
+// 		struct node*next;
+// 	};
+// 	struct node*head,*newnode;*/
+//     newnode=(struct node*)malloc(sizeof(struct node));
+    
+//     printf("enter data want to insert");
+//     scanf("%d",&newnode->data);
+    
+//     newnode->next=head;
+//     head=newnode;
+// }
+int main()
+{
+	create();
+// 	insert1();
+	if(head==NULL)
+	{
+// 		printf("list is empty");
+        cout<<"empty";
+	}
+	else
+	{
+	temp=head;
+	 while(temp!=NULL)
+	{
+// 		printf("%d ",temp->data);
+        cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
+return 0;
+}
