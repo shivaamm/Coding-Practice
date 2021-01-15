51f2cefce2ec6e862d53a83725a6853825ee5368
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-16 00:37:36 +0530

    Remove duplicates from an unsorted linked list (Using two loops)

---
+++ Linked List/unsorted_remove_duplicates.cpp
@@ -0,0 +1,33 @@
+// Using two loops method
+
+
+void removeDuplicates(struct Node *start)
+{
+    struct Node *ptr1, *ptr2, *dup;
+    ptr1 = start;
+ 
+    /* Pick elements one by one */
+    while (ptr1 != NULL && ptr1->next != NULL)
+    {
+        ptr2 = ptr1;
+ 
+        /* Compare the picked element with rest
+           of the elements */
+        while (ptr2->next != NULL)
+        {
+            /* If duplicate then delete it */
+            if (ptr1->data == ptr2->next->data)
+            {
+                /* sequence of steps is important here */
+                dup = ptr2->next;
+                ptr2->next = ptr2->next->next;
+                delete(dup);
+            }
+            else /* This is tricky */
+                ptr2 = ptr2->next;
+        }
+        ptr1 = ptr1->next;
+    }
+}
+
+// Time comp = O(N²)
