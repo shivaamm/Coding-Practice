0c3b0684b492f0a950e0ccce7630528cf6d5d7a9
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-16 04:06:51 +0530

    sorting of linked list

---
+++ Linked List/sorting.cpp
@@ -0,0 +1,37 @@
+/* Bubble sort the given linked list */
+void bubbleSort(struct Node *start) 
+{ 
+    int swapped, i; 
+    struct Node *ptr1; 
+    struct Node *lptr = NULL; 
+  
+    /* Checking for empty list */
+    if (start == NULL) 
+        return; 
+  
+    do
+    { 
+        swapped = 0; 
+        ptr1 = start; 
+  
+        while (ptr1->next != lptr) 
+        { 
+            if (ptr1->data > ptr1->next->data) 
+            {  
+                swap(ptr1, ptr1->next); 
+                swapped = 1; 
+            } 
+            ptr1 = ptr1->next; 
+        } 
+        lptr = ptr1; 
+    } 
+    while (swapped); 
+} 
+  
+/* function to swap data of two nodes a and b*/
+void swap(struct Node *a, struct Node *b) 
+{ 
+    int temp = a->data; 
+    a->data = b->data; 
+    b->data = temp; 
+} 
