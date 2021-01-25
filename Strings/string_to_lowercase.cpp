35bf9d065a4127bbbe6b1e02cc7c075ea45f83e7
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-26 02:14:46 +0530

    String To Lowercase

--- Strings/string_to_lowercase.cpp
+++ Strings/string_to_lowercase.cpp
@@ -1,4 +1,22 @@
 class Solution {
+public:
+    string toLowerCase(string str) {
+        int i=0;
+        char c;
+        string res;
+          while (str[i])
+          {
+            c=str[i];
+           res.push_back(tolower(c));
+            i++;
+          }
+        return res;
+    }
+};
+
+// using return ----------->>>>>>>>>>>
+
+class Solution {
   public:
     string toLower(string S) {
     // int res=transform(S.begin(), S.end(), S.begin() ::tolower)
