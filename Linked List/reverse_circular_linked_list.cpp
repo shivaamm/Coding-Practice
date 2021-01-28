54ee5261438d2a65d76f5514233e7d1b6c27b66f
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-01-28 22:49:22 +0530

    Circular Linked List in Reverse Program In C

---
+++ Linked List/reverse_circular_linked_list.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+   int data;
+   struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+//insert link at the first location
+void insert(int data) {
+   // Allocate memory for new node;
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+
+   link->data = data;
+   link->next = NULL;
+
+   // If head is empty, create new list
+   if(head==NULL) {
+      head = link;
+      head->next = link;
+      return;
+   }
+
+   current = head;
+   
+   // move to the end of the list
+   while(current->next != head)
+      current = current->next;
+
+   // Insert link at the end of the list
+   current->next = link;
+   
+   // Link the last node back to head
+   link->next = head;
+   
+}
+
+//display the list
+void reverse_print(struct node *list) {
+   if(list->next == head) {
+      printf(" %d =>",list->data);
+      return;
+   }
+   reverse_print(list->next);
+   printf(" %d =>",list->data);
+   
+}
+
+int main() {
+   insert(10);
+   insert(20);
+   insert(30);
+   insert(1);
+   insert(40);
+   insert(56); 
+   
+   reverse_print(head);
+   printf(" [head]\n");
+   
+   return 0;
+}
