0cafbc76f20baf31003b958ae74df6ce4755305e
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-03-10 00:12:03 +0530

    Count Leaves in Binary Tree

--- Trees/count_leaves_nodes_in_tree.cpp
+++ Trees/count_leaves_nodes_in_tree.cpp
@@ -1,3 +1,12 @@
+Algorithm 
+
+getLeafCount(node)
+1) If node is NULL then return 0.
+2) Else If left and right child nodes are NULL return 1.
+3) Else recursively calculate leaf count of the tree using below formula.
+    Leaf count of a tree = Leaf count of left subtree + 
+                                 Leaf count of right subtree
+
 int countLeaves(Node* node)
 {
     if(node == NULL)      
