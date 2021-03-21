106f48ad1ade7c85697ffcddc0fcd0bcebcb8391
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-03-21 20:39:01 +0530

    (A) Prison Break

---
+++ Codeforces/1484A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int a,b;
+    cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+    return 0;
+}
