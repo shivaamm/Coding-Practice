f3778c934d96605e9907191f8c972c131fa67a69
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-04-10 01:12:59 +0530

    Sum of two numbers without using arithmetic operators

---
+++ Bit Manipulation/sum_without_using_arithmetic_operators.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    int sum(int x , int y)
+    {
+        // Iterate till there is no carry
+        while (y != 0)
+        {
+            // carry now contains common
+            //set bits of x and y
+            int carry = x & y;
+     
+            // Sum of bits of x and y where at
+            //least one of the bits is not set
+            x = x ^ y;
+     
+            // Carry is shifted by one so that adding
+            // it to x gives the required sum
+            y = carry << 1;
+        }
+        return x;
+    }
+};
