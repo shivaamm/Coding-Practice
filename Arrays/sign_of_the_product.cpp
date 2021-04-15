f4509f41b7790d60b78836e8466c0bd7788a4025
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-04-16 01:18:18 +0530

    1822. Sign of the Product of an Array

---
+++ Arrays/sign_of_the_product.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int arraySign(vector<int>& v) {
+        
+        int c=0;
+        for(int i=0;i<v.size();i++)
+        {
+            if(v[i]==0)
+                return 0;
+            if(v[i]<0)
+                 c++;
+        }
+        if(c%2==0)
+            return 1;
+        else
+            return -1;
+        
+         // long long ans = accumulate(v.begin(), v.end(), 1, multiplies<int>());
+     
+//         int multi = 1;
+
+//         for (auto& e: v)
+//             multi *= e;
+        
+        // if(ans>0)
+        //     return 1;
+        // else if(ans<0)
+        //         return -1;
+        // else
+        //     return 0;
+    }
+};
