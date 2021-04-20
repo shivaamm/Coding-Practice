f0899acb83649d42dd3200f4fe753f2fc43cc9f6
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-04-20 23:51:15 +0530

    1094. Car Pooling

--- greedy/car_pooling.cpp
+++ greedy/car_pooling.cpp
@@ -1,3 +1,14 @@
+trips = [[2,1,5],[3,3,7]], capacity = 4
+After iterating over the trips array the map will look like this:
+
+{
+	1: 2,
+	3: 3,
+	5: -2,
+	7: -3
+}
+here 1,3,5,7 are the pickup and 2,3,-2,-3 are drop points for passengers that we are storing using maps.
+
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
@@ -10,7 +21,7 @@ public:
         
         int currentCapacity = 0;
         for (auto it : m) {
-            currentCapacity += it.second;
+            currentCapacity += it.second;   // here we check each time if for any iteration the total is greater than capacity then it will break the loop and return true.
             if (currentCapacity > capacity) {
                 return false;
             }
