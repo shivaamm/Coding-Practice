a946bc22e4e9a916330fbed4355c55351afbe2d2
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-04-25 02:52:51 +0530

    1338. Reduce Array Size to The Half

---
+++ Hashing/reduce_array_size_half.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        
+        map<int,int> hash;
+        vector<int> vec;
+        int ans = 0, s = 0;
+        for(int i:arr) hash[i]++;
+        for(auto [a,b]:hash) vec.push_back(b);
+        sort(vec.begin(),vec.end(),greater<int>());
+        for(int i=0;i<vec.size() && s*2<arr.size(); ++i) ans++, s += vec[i];
+        return ans;
+        
+        
+        // unordered_map<int, int> mp;
+        // for (int &n : arr) {
+        //     mp[n]++;
+        // }
+        // int half = arr.size() / 2;
+        // vector<int> counts;
+        // for (auto it : mp) {
+        //     counts.push_back(it.second);
+        // }
+        // if (counts.size() == 1) {
+        //     return 1;
+        // }
+        // sort(begin(counts), end(counts));
+        // int result = 0;
+        // long long int sum = 0;
+        // for (int i = counts.size() - 1; i >= 0; i--) {      //greedily choose the highest frequency till count reaches greater than or equal to half the size of the initial array.
+        //     if (sum < half) {
+        //         sum += counts[i];
+        //         result++;
+        //     }
+        // }
+        // return result;
+    }
+};
