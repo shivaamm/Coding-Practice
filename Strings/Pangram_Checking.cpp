fcba285c7bcdc6f348c9a653e363f4066e8172ac
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-04-25 04:13:10 +0530

    Pangram Checking (gfg)

---
+++ Strings/Pangram_Checking.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+    
+    // const int CHAR=256;
+    bool checkPangram (string &str)
+    {
+        // unordered_map<char,int>map;
+        // int s=str.size(),t=0;
+        // for(int i=0;i<s;i++)
+        //     map[str[i]]++;
+        // unordered_map<char,int>::iterator it;
+        // for(it=map.begin();it!=map.end();it++)
+        // {    if((it->second)>0)
+        //             t++;
+        // }
+        // if(t>=26)
+        //     return 1;
+        // else return 0;
+        
+      *******************************************************
+      
+        // if(str.size()<26)return false;
+        // int count[CHAR]={0};
+        // for(char c:str){
+        //     if(isalpha(c)){
+        //         count[tolower(c)]++;
+        //     }
+        // }
+        // for(int i=97;i<=122;i++){
+        //     if(count[i]==0)
+        //     return false;
+        // }
+        // return true;
+        
+       ******************************************************
+        
+        int i;
+        int h[26]={0};
+        
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+        for(i=0; i<str.length(); i++){
+            h[str.at(i)-'a']++;
+        }
+            for(i=0; i<26;i++){
+                if(h[i]==0) 
+                return false;
+            }
+            return true; 
+    }
+
+};
