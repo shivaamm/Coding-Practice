84c86faac46623dc1141c4f3501d221647673db4
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-05-06 00:21:37 +0530

    Minimum delete operations to make all elements of array same

---
+++ Hashing/min_operations_to_make_same_array.cpp
@@ -0,0 +1,38 @@
+In this problem, we need to minimize the delete operations. The approach is simple, we count the frequency of each element in an array, 
+then find the frequency of the most frequent elements in count array. Let this frequency be max_freq. 
+To get the minimum number of elements to be deleted from the array calculate n – max_freq where n is a number of elements in the given array.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to get minimum number of elements to be deleted
+// from array to make array elements equal
+int minDelete(int arr[], int n)
+{
+	// Create an hash map and store frequencies of all
+	// array elements in it using element as key and
+	// frequency as value
+	unordered_map<int, int> freq;
+	for (int i = 0; i < n; i++)
+		freq[arr[i]]++;
+
+	// Find maximum frequency among all frequencies.
+	int max_freq = INT_MIN;
+	for (auto itr = freq.begin(); itr != freq.end(); itr++)
+		max_freq = max(max_freq, itr->second);
+
+	// To minimize delete operations, we remove all
+	// elements but the most frequent element.
+	return n - max_freq;
+}
+
+// Driver program to run the case
+int main()
+{
+	int arr[] = { 4, 3, 4, 4, 2, 4 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << minDelete(arr, n);
+	return 0;
+}
