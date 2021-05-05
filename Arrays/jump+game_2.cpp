f8880e03fdedc53a4d8a06df2c1743ec9707e1b1
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-05-05 16:47:39 +0530

    45. Jump Game II

---
+++ Arrays/jump+game_2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int jump(vector<int>& N) {
+        int len = N.size() - 1, curr = -1, next = 0, ans = 0;
+        for (int i = 0; next < len; i++) {
+            if (i > curr) ans++, curr = next;
+            next = max(next, N[i] + i);
+        };
+        return ans;
+    }
+};
