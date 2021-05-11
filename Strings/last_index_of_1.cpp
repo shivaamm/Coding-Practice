c1307e4947f0cbd30ec564498139235b9facc9a4
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-05-12 00:28:37 +0530

    Last index of One

---
+++ Strings/last_index_of_1.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+    int lastIndex(string s) 
+    {
+        
+        // ************************** //
+        int flag=0;
+        int ind=0;
+        for(int i=0;i<s.length();i++)
+         {
+             if(s.at(i)=='1'){
+                ind=i;
+                flag=1;
+             }
+         }
+         if(flag==1)
+         return ind;
+         else
+         return -1;
+    }
+
+};
