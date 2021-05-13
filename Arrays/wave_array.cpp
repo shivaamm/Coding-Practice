55ccf806e4347a93c58d9d3cdef7c908fd3f47a7
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-05-14 01:47:43 +0530

    Wave Array

--- Arrays/wave_array.cpp
+++ Arrays/wave_array.cpp
@@ -3,6 +3,22 @@ class Solution{
     // Function to convert given array to wave like array
     // arr: input array
     // n: size of array
+    
+    void convertToWave(int *arr, int n){
+    
+    // Your code here
+        if(n == 1){
+            return;
+        }
+        for(int i = 0; i < n-1; i += 2){
+            swap(arr[i], arr[i+1]);
+        }
+    }
+    
+    
+    ****************************************************************
+        
+    
     void convertToWave(int *arr, int n){
         
        sort(arr, arr + n);
