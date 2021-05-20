5c1c76973afcaa32260dced88b96fb963ead2656
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-05-20 23:42:55 +0530

    Intersection Point in Y Shapped Linked Lists

---
+++ Linked List/intersection_point_in_Y_shaped.cpp
@@ -0,0 +1,15 @@
+int intersectPoint(Node* headA, Node* headB)
+{
+    if(headA == NULL || headB == NULL)
+            return NULL;
+        Node *a = headA , *b = headB;
+
+        while(a != b){
+
+            a = (a) ? a->next: headB;
+            b = (b) ? b->next: headA;
+
+        }
+        int ans = a->data;
+        return ans;
+}
