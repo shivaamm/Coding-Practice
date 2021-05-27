b9358603b6d816ac50a4dc0b7cc906bdd781dcdf
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-05-28 03:33:38 +0530

    6. Fibonacci Using Recursion

---
+++ Bit Manipulation/Mathematics & Recursion/fibonacci_using_recursion.cpp
@@ -0,0 +1,11 @@
+class Solution{
+    public:
+    //Complete this function
+    int fibonacci(int n)
+    {
+        if(n==0||n==1)
+            return n;
+            
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+};
