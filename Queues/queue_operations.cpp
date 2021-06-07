5141e5de83bfe6ad0830b96148f7c4683af2a6d4
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-06-07 22:52:48 +0530

    Queue Operations

---
+++ Queues/queue_operations.cpp
@@ -0,0 +1,26 @@
+class Solution{
+    public:
+    // Function to insert element into the queue
+    void insert(queue<int> &q, int k){
+        // Your code here
+        q.push(k);
+    }
+    
+    // Function to find frequency of an element
+    // return the frequency of k
+    int findFrequency(queue<int> &q, int k){
+        int z=0;
+        queue<int> n1;
+        while(!q.empty())
+        {
+            if(q.front()==k)
+            z++;
+            n1.push(q.front());
+            q.pop();
+        }
+        q=n1;
+        if(z==0)return -1;
+        return z;
+    }
+    
+};
