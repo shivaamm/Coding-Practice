3eb6def1539b94e0f34fdbd3a332323547bd241b
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-06-08 21:49:47 +0530

    Twice Counter

---
+++ Strings/twice_counter.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+        int countWords(string list[], int n)
+        {
+             unordered_map<string, int>mp; 
+           for(int i =0; i < n; i++)
+                mp[list[i]]++;
+            int counter = 0; 
+            for(auto x : mp)
+            {
+                if(x.second == 2)
+                    counter++; 
+            }
+            return counter; 
+        }
+
+};
