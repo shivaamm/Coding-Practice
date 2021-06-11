e6092eb22ff88e8df373337a4534328dd1e26f89
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-06-11 19:21:13 +0530

    Remove loop in Linked List

--- GeeksforGeeks/remove_loop_linked_list.cpp
+++ GeeksforGeeks/remove_loop_linked_list.cpp
@@ -1,7 +1,20 @@
 https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1/?problemStatus=unsolved&page=1&category[]=Linked%20List&query=problemStatusunsolvedpage1category[]Linked%20List#
 
-#include<unordered_map>
-typedef Node node;
+Input:
+N = 3
+value[] = {1,3,4}
+X = 2
+Output: 1
+Explanation: The link list looks like
+1 -> 3 -> 4
+     ^    |
+     |____|    
+A loop is present. If you remove it 
+successfully, the answer will be 1. 
+
+
+#include<unordered_map>    
+typedef Node node;                // declare globally otherwise it will show error
 void removeLoop(Node* head)
 {
     if (head == NULL || head -> next == NULL)
