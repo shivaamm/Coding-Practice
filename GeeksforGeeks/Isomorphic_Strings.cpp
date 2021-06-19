a17664ceb5a31e33e1e5580260299e76612d803a
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-06-20 01:36:47 +0530

    Isomorphic Strings

---
+++ GeeksforGeeks/Isomorphic_Strings.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+    //Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2)
+{
+ 
+    int m = str1.length(), n = str2.length();
+ 
+    // Length of both strings must be same for one to one
+    // corresponance
+    if (m != n)
+      return false;
+ 
+    // To mark visited characters in str2
+    bool marked[MAX_CHARS] = {false};
+ 
+    // To store mapping of every character from str1 to
+    // that of str2. Initialize all entries of map as -1.
+    int map[MAX_CHARS];
+    memset(map, -1, sizeof(map));
+ 
+    // Process all characters one by on
+    for (int i = 0; i < n; i++)
+    {
+        // If current character of str1 is seen first
+        // time in it.
+        if (map[str1[i]] == -1)
+        {
+            // If current character of str2 is already
+            // seen, one to one mapping not possible
+            if (marked[str2[i]] == true)
+                return false;
+ 
+            // Mark current character of str2 as visited
+            marked[str2[i]] = true;
+ 
+            // Store mapping of current characters
+            map[str1[i]] = str2[i];
+        }
+ 
+        // If this is not first appearance of current
+        // character in str1, then check if previous
+        // appearance mapped to same character of str2
+        else if (map[str1[i]] != str2[i])
+            return false;
+    }
+ 
+    return true;
+}
+ 
+};
