a553d541c1707f4811399e4a5987fdf64a53e10a
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-04 03:15:37 +0530

    Coin change MINIMUM no of coins ( UNBOUNDED KNAPSACK)

--- Dynamic Programming/coin_change_minimum_coins.cpp
+++ Dynamic Programming/coin_change_minimum_coins.cpp
@@ -58,6 +58,41 @@ public:
 };
 
 
+******************************************* even without doing the initialization for 2nd row code will work  ***************************************
+
+
+int coinChange(vector<int>& coins, int amount) 
+    {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        for(int i=0;i<=amount;i++)
+        {
+            dp[0][i]=INT_MAX-1;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount]>=INT_MAX-1?-1:dp[n][amount];
+    }
+
+
+
+
 ///////////////////////////////////////////////////////////////  GFG   ///////////////////////////////////////////////////////////////////////////
 
 
