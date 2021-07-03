233ebd5752b660692ab7e7b32508379ea44b98c5
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-04 03:09:55 +0530

    Coin change MINIMUM no of coins ( UNBOUNDED KNAPSACK)

--- Dynamic Programming/coin_change_minimum_coins.cpp
+++ Dynamic Programming/coin_change_minimum_coins.cpp
@@ -1,3 +1,128 @@
+https://leetcode.com/problems/coin-change/
+https://practice.geeksforgeeks.org/problems/number-of-coins1824/1#
+
+///////////////////////////////////////////////////   leetcode        //////////////////////////////////////////////////////
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& nums, int amount) {
+        int n=nums.size();
+	    int t[n+1][amount+1];
+	    
+	    for(int i=0;i<n+1;i++)
+	    {
+	        for(int j=0;j<amount+1;j++)
+	        {
+	            if(i==0)
+	            {
+	                t[i][j]=INT_MAX-1;
+	            }
+	           if(j==0 && i==1|| j==0)
+	           {
+	               t[i][j]=0;
+	           }
+	           if(i==1)
+	           {
+	               if(j%nums[0]==0)
+	               {
+	                   t[i][j]=j/nums[0];
+	               }
+	               else
+	               {
+	                   t[i][j]=INT_MAX-1;
+	               }
+	           }
+	        }
+	    }
+	    for(int i=2;i<n+1;i++)
+	    {
+	        for(int j=1;j<amount+1;j++)
+	        {
+	            if(nums[i-1]<=j)
+	            {
+	                t[i][j]=min(1+t[i][j-nums[i-1]],t[i-1][j]);
+	            }
+	            else
+	            {
+	                t[i][j]=t[i-1][j];
+	            }
+	        }
+	    }
+	    if(t[n][amount]==INT_MAX-1)
+	    {
+	        return -1;
+	    }
+	    return t[n][amount];
+    }
+};
+
+
+///////////////////////////////////////////////////////////////  GFG   ///////////////////////////////////////////////////////////////////////////
+
+
+class Solution{
+
+	public:
+// 	int minCoins(int coins[], int M, int V) 
+	int minCoins(int nums[], int n, int amount) 
+	{ 
+	   //int n=nums.size();
+	    int t[n+1][amount+1];
+	    
+	    for(int i=0;i<n+1;i++)
+	    {
+	        for(int j=0;j<amount+1;j++)
+	        {
+	            if(i==0)
+	            {
+	                t[i][j]=INT_MAX-1;
+	            }
+	           if(j==0 && i==1|| j==0)
+	           {
+	               t[i][j]=0;
+	           }
+	           if(i==1)
+	           {
+	               if(j%nums[0]==0)
+	               {
+	                   t[i][j]=j/nums[0];
+	               }
+	               else
+	               {
+	                   t[i][j]=INT_MAX-1;
+	               }
+	           }
+	        }
+	    }
+	    for(int i=2;i<n+1;i++)
+	    {
+	        for(int j=1;j<amount+1;j++)
+	        {
+	            if(nums[i-1]<=j)
+	            {
+	                t[i][j]=min(1+t[i][j-nums[i-1]],t[i-1][j]);
+	            }
+	            else
+	            {
+	                t[i][j]=t[i-1][j];
+	            }
+	        }
+	    }
+	    if(t[n][amount]==INT_MAX-1)
+	    {
+	        return -1;
+	    }
+	    return t[n][amount];
+	} 
+	  
+};
+
+
+///////////////////////////////////////////////////////////////  GFG   ///////////////////////////////////////////////////////////////////////////
+
+**********************************************************   OTHER WAY  *************************************************************
+
 class Solution
 {
     public:
