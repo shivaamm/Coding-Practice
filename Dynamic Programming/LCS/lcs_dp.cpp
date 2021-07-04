55a98b112e221076eabeb2066b8027fa988e7258
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-05 03:08:52 +0530

    Longest Common Subsequence (DP TOP DOWN)
    https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1

---
+++ Dynamic Programming/LCS/lcs_dp.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+    public:
+    
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+         int t[x+1][y+1];
+        memset(t,-1,sizeof(t));
+        
+        for(int i=0;i<x+1;i++)
+        {
+            for(int j=0;j<y+1;j++)
+            {
+                if(i==0||j==0)
+                {
+                    t[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<x+1;i++)
+        {
+            for(int j=1;j<y+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else
+                {
+                    t[i][j]=max(t[i][j-1],t[i-1][j]);
+                }
+            }
+        }
+        return t[x][y];
+    }
+};
