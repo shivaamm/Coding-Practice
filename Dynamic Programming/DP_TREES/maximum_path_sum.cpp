1d954adc51b03007fa6e65b13d458f1756ce1b93
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-11 03:34:56 +0530

    124. Binary Tree Maximum Path Sum [ from any node to any ] (using DP)

---
+++ Dynamic Programming/DP_TREES/maximum_path_sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int res= INT_MIN;
+    
+    int solve(TreeNode* root) {
+        
+        if (root == NULL)    ////////////////////////  Base Condition
+            return 0;
+         
+        int l = Helper(root->left);         /////////////////////  Hypothesis
+        int r = Helper(root->right);
+        
+        int temp = max(root->val + max(l, r), root->val);          /////// Induction
+        int ans = max(temp, l + r + root->val);
+        res = max(res, ans);
+        
+        return temp;
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        solve(root);
+        return res;
+    }
+};
