a8209333e84f87a61466f03703c5b2c49a194f3e
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-13 00:07:30 +0530

    Parenthesis Checker

--- Strings/Parenthesis_Checker.cpp
+++ Strings/Parenthesis_Checker.cpp
@@ -1,6 +1,48 @@
 class Solution
 {
     public:
+    bool compare(char a,char b)
+    {
+        
+        return ((a=='('&&b==')')||(a=='{'&&b=='}')||(a=='['&&b==']'));
+        
+    }
+    
+    bool ispar(string x)
+    {
+     stack<int> st;
+     for(int i=0;i<x.length();i++)
+     {
+         if(x[i]=='('||x[i]=='{'||x[i]=='[')
+           {
+               st.push(x[i]);
+            }
+         if(x[i]==')'||x[i]=='}'||x[i]==']')
+         {
+             if(st.empty()==true)
+               return false;
+             
+             if(compare(st.top(),x[i]))
+             {
+                 st.pop();
+             }
+             else
+               return false;
+                
+         }
+         
+     }
+     return st.empty();
+    }
+};
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Solution
+{
+    public:
     //Function to check if brackets are balanced or not.
     bool ispar(string expr)
     {
