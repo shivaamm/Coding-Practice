8b25ea55ed4f465f39c7fc705d31acbdfd3ec9af
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-12 23:06:53 +0530

    205. Isomorphic Strings

--- Strings/isomorphic_strings.cpp
+++ Strings/isomorphic_strings.cpp
@@ -17,3 +17,27 @@ public:
         return true;
     }
 };
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Solution {
+public:
+   string normalize_pattern(string word) {
+        unordered_map<char, char> m;
+        char curr = 'a';
+        
+        for (auto letter : word)
+            if (!m[letter]) m[letter] = curr++;
+        
+        for (int i = 0; i < word.size(); i++) word[i] = m[word[i]];
+        
+        return word;
+        
+    }
+    
+    bool isIsomorphic(string s, string t) {
+        return normalize_pattern(s) == normalize_pattern(t);
+    }
+};
