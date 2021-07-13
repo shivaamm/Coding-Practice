8c3b585d5adc985df5550946defc81ae30de5257
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-13 15:35:58 +0530

    162. Find Peak Element

--- Arrays/find_peak_element.cpp
+++ Arrays/find_peak_element.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        //  int l = 0, r = nums.size();
-        // if(r == 1)
-        //     return 0;
-        // while(l < r){
-        //     int mid = l + (r - l) / 2;
-        //     if(mid + 1 < nums.size() and nums[mid] < nums[mid + 1])
-        //         l = mid + 1;
-        //     else
-        //         r = mid;
-        // }
-        // return l;
+         int l = 0, r = nums.size();
+        if(r == 1)
+            return 0;
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(mid + 1 < nums.size() and nums[mid] < nums[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
         
         
         
