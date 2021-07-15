b1fa127e6bfc403f7bebd2d9f684af3c1a0a431b
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-15 23:48:47 +0530

    Permutations of a given string (using nextpermutation and 2nd method-swap)

--- Strings/permutations_of+string.cpp
+++ Strings/permutations_of+string.cpp
@@ -1,18 +1,18 @@
 class Solution
 {
 	public:
-// 		vector<string>find_permutation(string s)
-// 		{
-// 		    vector<string>v;
-// 		   sort(s.begin(),s.end());
-//     	    do
-//     	    {
-//     	        v.push_back(s);
-//     	    }
-//     	    while(next_permutation(s.begin(),s.end()));
+		vector<string>find_permutation(string s)
+		{
+		    vector<string>v;
+		   sort(s.begin(),s.end());
+    	    do
+    	    {
+    	        v.push_back(s);
+    	    }
+    	    while(next_permutation(s.begin(),s.end()));
     	    
-//     	    return v;
-// 		}
+    	    return v;
+		}
 
 
 
