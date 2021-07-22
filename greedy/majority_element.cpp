173a3a646498c0e4d797bea242942b9ba2312738
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-07-22 23:00:51 +0530

    Majority Element

--- greedy/majority_element.cpp
+++ greedy/majority_element.cpp
@@ -21,33 +21,35 @@ class Solution{
 	    else{
 	        return -1;
 	    }
+	    
+	    ********************************************************************************
         
-        // int maxCount = 0;
-        // int index = -1; // sentinels
-        // for (int i = 0; i < n; i++) {
-        //     int count = 0;
-        //     for (int j = 0; j < n; j++) {
-        //         if (arr[i] == arr[j])
-        //             count++;
-        //     }
+        int maxCount = 0;
+        int index = -1; // sentinels
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j])
+                    count++;
+            }
      
-        //     // update maxCount if count of
-        //     // current element is greater
-        //     if (count > maxCount) {
-        //         maxCount = count;
-        //         index = i;
-        //     }
-        // }
+            // update maxCount if count of
+            // current element is greater
+            if (count > maxCount) {
+                maxCount = count;
+                index = i;
+            }
+        }
      
-        // // if maxCount is greater than n/2
-        // // return the corresponding element
-        // if (maxCount > n / 2)
-        //     return arr[index];
-        // else
-        // {
-        //     // cout<<"-1";
-        //     return -1;
-        // }
+        // if maxCount is greater than n/2
+        // return the corresponding element
+        if (maxCount > n / 2)
+            return arr[index];
+        else
+        {
+            // cout<<"-1";
+            return -1;
+        }
             
     }
 };
