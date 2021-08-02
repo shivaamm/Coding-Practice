f74c19e816be593e7caca521212fb176a469ac7b
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-08-02 23:52:47 +0530

    Update two_sum.cpp

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    // vector<int> twoSum(vector<int>& nums, int target) {
-    //     vector<int> result;
-    // for (int i=0;i<nums.size()-1;i++){
-    //     for(int j=i+1;j<nums.size();j++){
-    //         if((nums[i]+nums[j])==target && i!=j){
-    //           result.push_back(i);
-    //           result.push_back(j);
-    //         }
-    //     }
-    // }
-    // return result;
-    // }
-    
-    
-    
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+    for (int i=0;i<nums.size()-1;i++){                           // O(n2) approach
+        for(int j=i+1;j<nums.size();j++){
+            if((nums[i]+nums[j])==target && i!=j){
+              result.push_back(i);
+              result.push_back(j);
+            }
+        }
+    }
+    return result;
+    }
+    
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+     
+	 using hashing
+	
+    vector<int> twoSum(vector<int>& nums, int target) { 
 		// Keys are the array values and values are the associated array indices
         unordered_map<int, int> hash; // Use as a hash table, instead of creating one
         vector<int> result;
