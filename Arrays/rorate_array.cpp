7426683dfdb69983472cbeb0687ad1dfb95c53f5
Shivam Singhal <shivam.singhal04888@gmail.com>
2021-12-12 22:42:49 +0530

    189. Rotate Array

---
+++ Arrays/rorate_array.cpp
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/rotate-array/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %=nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
