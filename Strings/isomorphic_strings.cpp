b0610da7f0a1922e311b949c4126b8b99a0443d6
Shivam Singhal <shivam.singhal04888@gmail.com>
2022-01-20 00:13:45 +0530

    205. Isomorphic Strings

--- Strings/isomorphic_strings.cpp
+++ Strings/isomorphic_strings.cpp
@@ -1,23 +1,67 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-         int arr1[256] = {};
-        int arr2[256] = {};
-        int len = s.length();
-        for(int i =0;i<len;i++) {
-           char c1 = s[i];
-           char c2 = t[i];
-            if(arr1[c1]!=arr2[c2]) {
-              return false;
-           }
-            arr1[c1] = i+1;
-            arr2[c2] = i+1;
-        }
         
+        // ALL CORRECT /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        
+        
+//         unordered_map<char, char> ht1, ht2;
+//         int n=s.size();
+        
+//         for(int i=0; i<n; i++){
+//             if(ht1.find(s[i]) == ht1.end()){
+//                 if(ht2.find(t[i]) != ht2.end()){
+//                     return false;
+//                 }
+//                 ht1[s[i]] = t[i];
+//                 ht2[t[i]] = s[i];
+//             }
+//             else if(ht1[s[i]] != t[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        
+         if(s.size() != t.size())
+            return false;
+        unordered_map<char, char> m1;
+        unordered_map<char, char> m2;
+        for(int i = 0; i < s.size(); i++){
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
+        }
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(m1[s[i]] != t[i] || m2[t[i]] != s[i])
+                return false;
+        }
+            
         return true;
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        
+//          int arr1[256] = {};
+//         int arr2[256] = {};
+//         int len = s.length();
+//         for(int i =0;i<len;i++) {
+//            char c1 = s[i];
+//            char c2 = t[i];
+//             if(arr1[c1]!=arr2[c2]) {
+//               return false;
+//            }
+//             arr1[c1] = i+1;
+//             arr2[c2] = i+1;
+//         }
+        
+//         return true;
     }
 };
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
