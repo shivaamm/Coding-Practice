522bc577813189221b5b3e86db1d05212a0cacc0
Shivam Singhal <shivam.singhal04888@gmail.com>
2023-05-30 00:12:07 +0530

    Create CamelCase Pattern Matching.cpp
    GFG- https://practice.geeksforgeeks.org/problems/camelcase-pattern-matching2259/1

---
+++ Strings/CamelCase Pattern Matching.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<string> CamelCase(int n, vector<string> Dictionary, string pattern) {
+        vector<string> ans;
+        for(auto current : Dictionary){
+            
+            string need = "";
+            for(auto i : current)
+                if(isupper(i))
+                    need += i;
+                    
+            bool ok = 1;
+            if(need.size() < pattern.size())
+                continue;
+                
+            for(int i = 0; i < pattern.size(); i++){
+                if(need[i] != pattern[i]){
+                    ok = 0;
+                    break;
+                }
+            }
+            
+            if(ok)
+                ans.push_back(current);
+        }
+        
+        if(ans.size() == 0)
+            ans.push_back("-1");
+            
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<string> Dictionary(N);
+        for (int i = 0; i < N; i++) cin >> Dictionary[i];
+        string Pattern;
+        cin >> Pattern;
+        Solution ob;
+        vector<string> ans = ob.CamelCase(N, Dictionary, Pattern);
+        sort(ans.begin(), ans.end());
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
