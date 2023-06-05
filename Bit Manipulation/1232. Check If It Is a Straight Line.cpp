60d4670daa8737073df1d8bdd85b72960d698ccb
Shivam Singhal <shivam.singhal04888@gmail.com>
2023-06-05 19:57:34 +0530

    1232. Check If It Is a Straight Line.cpp

---
+++ Bit Manipulation/1232. Check If It Is a Straight Line.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+        
+        for (int i = 2; i < coordinates.size(); i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+            if ((x - x0) * (y1 - y0) != (y - y0) * (x1 - x0)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
