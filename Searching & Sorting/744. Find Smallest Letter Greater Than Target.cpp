652048972f282a8227224192b1866a8a6cc5cd0d
Shivam Singhal <shivam.singhal04888@gmail.com>
2023-06-09 11:46:55 +0530

    744. Find Smallest Letter Greater Than Target.cpp

---
+++ Searching & Sorting/744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0;
+        int right = letters.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left < letters.size() ? letters[left] : letters[0];
+    }
+};
