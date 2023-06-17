b27269fcd4c1b985d29cc9756e704d05cd36614f
Shivam Singhal <shivam.singhal04888@gmail.com>
2023-06-17 14:44:50 +0530

    queue_operations.cpp

--- Queues/queue_operations.cpp
+++ Queues/queue_operations.cpp
@@ -24,3 +24,25 @@ class Solution{
     }
     
 };
+
+
+****************************************************************************
+
+class Solution{
+    public:
+    // Function to insert element into the queue
+    unordered_map<int,int>mp;
+    void insert(queue<int> &q, int k){
+        // Your code here
+        q.push(k);
+        mp[k]++;
+    }
+    
+    // Function to find frequency of an element
+    // return the frequency of k
+    int findFrequency(queue<int> &q, int k){
+        // Your code here
+        return mp[k]==0?-1:mp[k];
+    }
+    
+};
