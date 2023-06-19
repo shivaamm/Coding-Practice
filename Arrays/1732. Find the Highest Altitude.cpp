9d5598e7e0a55206882aa57533ac804f993a9f3d
Shivam Singhal <shivam.singhal04888@gmail.com>
2023-06-19 10:01:55 +0530

    1732. Find the Highest Altitude.cpp

---
+++ Arrays/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int>v;
+        v.push_back(0);
+        int sum=0,c=0;
+        int n=gain.size();
+        for(int i=0;i<n;i++)
+        {
+            sum+=gain[i];
+            v.push_back(sum);
+        }
+        return *max_element(v.begin(),v.end()); // gives(searches) the max element from the vector from start to end
+    }
+};
