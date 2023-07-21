a61f3355dbce51b3d01d8d1ba461b29f5741fada
Shivam Singhal <shivam.singhal04888@gmail.com>
2023-07-21 09:10:36 +0530

    673. Number of Longest Increasing Subsequence.cpp

--- Dynamic Programming/673. Number of Longest Increasing Subsequence.cpp
+++ Dynamic Programming/673. Number of Longest Increasing Subsequence.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-
-        vector<int> dp(n, 1); 
-        int maxLength = 1; 
-        int result = 0; 
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    if (dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
+    int findNumberOfLIS(vector<int>& nums) 
+    {
+        int n = nums.size(), maxLISLen = 1;
+        
+        vector<int>length(n, -1), count(n, -1); //DP
+        //===================================================================================
+        for (int start = n - 1; start >= 0; start--)
+        {
+            int LISLen = 1, LISCount = 1;
+            //=======================================================
+            for (int i = start + 1; i < nums.size(); i++)
+            {
+                if (nums[i] > nums[start])
+                {
+                    int currLen = 1 + length[i]; //1 + nextLen
+                    if (currLen > LISLen)
+                    {
+                        LISLen = currLen;
+                        LISCount = count[i]; //nextCount
                     }
+                    else if (currLen == LISLen) 
+                        LISCount = LISCount + count[i];
                 }
             }
-            maxLength = max(maxLength, dp[i]);
+            //=============================================================
+            length[start] = LISLen; 
+            count[start] = LISCount;
+            maxLISLen = max(maxLISLen, length[start]);
         }
-
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == maxLength) {
-                result++;
-            }
+        //========================================================================================
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (length[i] == maxLISLen) 
+                ans += count[i];
         }
-
-        return result;
+        return ans; 
     }
 };
